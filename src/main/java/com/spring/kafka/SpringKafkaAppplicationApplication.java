8759b19500b3e537bab5ccab9700af214e471868
LordMaduz <maduz.ruchira@gmail.com>
2024-02-21 16:37:37 +0800

    Add other implementation.

---
+++ src/main/java/com/spring/kafka/SpringKafkaAppplicationApplication.java
@@ -0,0 +1,13 @@
+package com.spring.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaAppplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaAppplicationApplication.class, args);
+	}
+
+}
